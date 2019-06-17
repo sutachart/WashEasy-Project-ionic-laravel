dd1609f128d5a4705df3d8deff1128f30fdc406f
Mynky <sutachart1881@gmail.com>
2019-06-17 10:57:56 +0700

    add callwash

---
+++ src/app/callwash/callwash.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-callwash',
+  templateUrl: './callwash.page.html',
+  styleUrls: ['./callwash.page.scss'],
+})
+export class CallwashPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
