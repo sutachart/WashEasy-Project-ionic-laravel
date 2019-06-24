49fbb7568f2d1f371f85f4e38eed6e151b7849db
Mynky <sutachart1881@gmail.com>
2019-06-24 16:50:42 +0700

    new gps

--- src/app/user/gps-map/gps-map.page.ts
+++ src/app/user/gps-map/gps-map.page.ts
@@ -1,20 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+declare var google;
 
 @Component({
   selector: 'app-gps-map',
   templateUrl: './gps-map.page.html',
   styleUrls: ['./gps-map.page.scss'],
 })
-export class GpsMapPage implements OnInit {
+export class GpsMapPage  {
 
-  constructor(private router:Router) { }
+  MyLocation: any;
+  latitude: any;
+  longtitude: any;
 
-  ngOnInit() {
+
+  @ViewChild('mapElement') mapNativeElement: ElementRef;
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
+
+  constructor(private geolocation: Geolocation,private router:Router) { }
+
+  ngAfterViewInit(): void {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longtitude = resp.coords.longitude;
+      const options = {
+        zoom: 15,
+        mapTypeId: 'terrain'
+      };
+      const map = new google.maps.Map(this.mapNativeElement.nativeElement, options);
+      const infoWindow = new google.maps.InfoWindow;
+      const pos = {
+        lat: this.latitude,
+        lng: this.longtitude
+      };
+      
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('My Location');
+      infoWindow.open(map);
+      
+      console.log('Latitue :', this.latitude);
+      console.log('Longtitue :', this.longtitude);
+      map.setCenter(pos);
+      
+      this.directionsDisplay.setMap(map);
+
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
   }
 
-  addDetail(){
-    this.router.navigateByUrl("/location-add-detail");
-  }
 
 }
